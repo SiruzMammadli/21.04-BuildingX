866057ccc4e6e765322f60386d2098bf99760980
SiruzMammadli <siruz.mammadli@gmail.com>
2022-04-22 01:34:04 +0400

    update 22.04 01:33

--- js/main.js
+++ js/main.js
@@ -5,13 +5,24 @@ let headMidHeight = document.querySelector('header .mid--side').clientHeight;
 
 let headerHeight = headTopHeight + headMidHeight;
 
-let navbar = document.querySelector('header .navbar')
+let navbar = document.querySelector('header .navbar');
 
 window.onscroll = function() {
-    window.scrollY > headerHeight
-        ? navbar.classList.add('sticky')
-        : navbar.classList.remove('sticky')
+    if (window.outerWidth >= 850) {
+        window.scrollY > headerHeight
+            ? navbar.classList.add('sticky')
+            : navbar.classList.remove('sticky')
+    }
 }
 
 // End of Sticky Navbar
 
+// Loader Screen
+
+let loader = document.querySelector('.loader-screen');
+
+window.onload = function() {
+    loader.classList.add('hidden');
+}
+
+// End of Loader Screen
